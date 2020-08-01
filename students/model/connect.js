7204409ae6ec8e4c70b7ac93929069005ea0f01b
zhan <zhanmeikai@163.com>
2020-08-01 21:32:46 +0800

    模板引擎artTemplat

--- students/model/connect.js
+++ students/model/connect.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-// 数据库连接 27017是mongodb数据库的默认端口
+// 连接数据库
 mongoose.connect('mongodb://localhost/playground', { useNewUrlParser: true })
 	.then(() => console.log('数据库连接成功'))
-	.catch(() => console.log('数据库连接失败'));
+	.catch(() => console.log('数据库连接失败'))
